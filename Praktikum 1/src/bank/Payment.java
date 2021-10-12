01e196403a0c612b7426aaec393e4ba14f2ed5a3
Jay <57915168+JayTheferret@users.noreply.github.com>
2021-10-12 12:11:47 +0200

    added basic outline

---
+++ Praktikum 1/src/bank/Payment.java
@@ -0,0 +1,77 @@
+package bank;
+
+public class Payment {
+
+    //überlegen ob public oder private sinnvoll
+    String date; //Zeitpunkt -> Format: DD.MM.YYYY(muss nicht überprüft werden)
+    double amount; //Geldmenge (kann auch negativ sein (Auszahlung))
+    String description; //beschreibung des Vorgangs
+
+
+    //Zinsen -> pos Wert von 0 bis 1
+    double incomingInterest; //bei Einzahlung/Deposit
+    double outgoingInterest; //bei Auszahlung/Withdrawal
+
+    //getter and setter
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getIncomingInterest() {
+        return incomingInterest;
+    }
+    public void setIncomingInterest(double incomingInterest) {
+        this.incomingInterest = incomingInterest;
+    }
+
+    public double getOutgoingInterest() {
+        return outgoingInterest;
+    }
+    public void setOutgoingInterest(double outgoingInterest) {
+        this.outgoingInterest = outgoingInterest;
+    }
+
+    //constructor for attributes date, amount und description.
+    public Payment(String date, double amount, String description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public Payment(String date, double amount, String description, double incomingInterest, double outgoingInterest) {
+        this.Payment(date,amount,description); //use other constructor to not duplicate code
+        this.incomingInterest = incomingInterest;
+        this.outgoingInterest = outgoingInterest;
+    }
+
+    //copy constructor
+    Payment(Payment a){
+        date = a.date;
+        amount = a.amount;
+        description = a.description;
+        incomingInterest = a.incomingInterest;
+        outgoingInterest = a.outgoingInterest;
+    }
+
+    public void printObject(){
+        System.out.print("Date: ");
+        System.out.print(this.date);
+    };
+}
---
+++ Praktikum 1/src/bank/Transfer.java
@@ -0,0 +1,49 @@
+package bank;
+
+public class Transfer {
+
+    //ueberlegen ob public oder private sinnvoll
+    String date; //Zeitpunkt -> Format: DD.MM.YYYY (muss nicht ueberprueft werden)
+    double amount; //Geldmenge (nur positiv (-> muss nicht überprueft werden))
+    String description; //Beschreibung des Vorgangs
+
+    String sender; //Sender
+    String recipient; //Empfänger
+
+    //getter and setter
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+}
